45762a2c3b5eb0c961f0539d8a3f09e9c00d0f5c
robb666 <56496611+robb666@users.noreply.github.com>
2021-06-21 07:32:08 +0200

    Update Algorithms Full Course -Design and Analysis of Algorithms.py

--- Algorithms Full Course -Design and Analysis of Algorithms.py
+++ Algorithms Full Course -Design and Analysis of Algorithms.py
@@ -1,7 +1,24 @@
 
+
+def grade_mult(x, y):
+    result = []
+    for i in str(x):
+        for j in str(y):
+            h = int(i) * int(j)
+            result.insert(0, h)
+    return result
+
+
+y = 152
+x = 151
+print(grade_mult(x, y))
+
+
+
+
+
+
 """Karatsuba multiplication"""
-
-
 def Karatsuba(x, y):
     a = int(str(x)[:2])
     b = int(str(x)[2:])
@@ -12,14 +29,20 @@ def Karatsuba(x, y):
     step_2 = b * d
     step_3 = (a + b) * (c + d)
     step_4 = step_3 - step_2 - step_1
+    # step_5 =
 
-    return step_4
+    print(step_1)
+    print(step_2)
+    print(step_3)
+    print(step_4)
+
+    # return step_4
 
 
 
 
-x = 5678
-y = 1234
+# x = 5678
+# y = 1234
+# print(Karatsuba(x, y))
 
-
-print(Karatsuba(x, y))
+# print("1.50" == str(1.50))
