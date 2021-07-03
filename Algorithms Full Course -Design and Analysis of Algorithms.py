29bf505142e82066355b468990044c7865989315
robb666 <56496611+robb666@users.noreply.github.com>
2021-07-03 18:35:18 +0200

    Update Algorithms Full Course -Design and Analysis of Algorithms.py

--- Algorithms Full Course -Design and Analysis of Algorithms.py
+++ Algorithms Full Course -Design and Analysis of Algorithms.py
@@ -1,13 +1,20 @@
 
 
 def memory_add(result, memory):
-    for i in range(len(result)):
-        for j in range(len(result[0])):
-            result[i][j] = result[i][j] + memory[i][j]
+    memo = 0
+    for i in range(len(result) - 1, -1, -1):
+        for j in range(len(result[0]) - 1, -1, -1):
+            result[i][j] = memory[i][j] + result[i][j]
+            if result[i][j] > 9:
+                memo, result[i][j] = int(str(result[i][j])[0]), int(str(result[i][j])[1])
+                memory[i][j - 1] = memo + memory[i][j - 1]
+    if memory[0][0] + result[0][0] > 9:
+        result[0].insert(0, memo)
 
     return result
 
 
+def final_add(): pass
 
 
 def grade_mult(x, y):
@@ -17,24 +24,10 @@ def grade_mult(x, y):
     result = [[] for _ in range(len(y))]
     memory_1 = [[0] for _ in range(len(y))]
     fresult = [[] for _ in range(len(y))]
-    # trailing_zeros = []
-    #
-    # for i in reversed(x):
-    #     if i.startswith('0'):
-    #         x = y.replace('0', '')
-    #         trailing_zeros.append(0)
-    #     else:
-    #         break
-    #
-    # for i in reversed(y):
-    #     if i.startswith('0'):
-    #         y = y.replace('0', '')
-    #         trailing_zeros.append(0)
-    #     else:
-    #         break
-    #
-    # print(trailing_zeros)
+
     print(x, y)
+    k = 0
+    memo = 0
     for r_ind, i in enumerate(reversed(y)):
         [result[r_ind].append(0) for _ in range(r_ind) if r_ind > 0]
         for c_ind, j in enumerate(reversed(x)):
@@ -45,88 +38,41 @@ def grade_mult(x, y):
             k = k + memory_1[r_ind][c_ind]
             print(k)
             if k > 9:
-                # result[r_ind][-1] = 0
-                # result[r_ind].insert(0, 0)
-                # memory_1[r_ind].append(0)
                 memo, k = int(str(k)[:-1]), int(str(k)[-1])
                 result[r_ind].insert(0, k)
                 memory_1[r_ind].insert(0, memo)
-    #
-    #             print(memory_1)
-    #             print(result)
-    #
+
             else:
                 result[r_ind].insert(0, k)
                 memory_1[r_ind].insert(0, 0)
-                if len(x) - 1 == c_ind and len(result[r_ind]) < len(memory_1[r_ind]):
-                    result[r_ind].insert(0, 0)
+
+        if len(result[r_ind]) < len(memory_1[r_ind]):
+            result[r_ind].insert(0, 0)
+
+    """dodać jedno jak w result zero do pomieci z tyłu"""
+
+    # memory_add(result, memory_1)
 
 
-    if memory_1[0][0] > 0:
-        result[0].insert(0, memory_1[0][0])
+        # if len(x) - 1 == c_ind and len(result[r_ind]) > len(memory_1[r_ind]):
+        #     result[r_ind].insert(0, 0)
+
+    # for i in range(len(result)):
+    #     if memo + memory_1[0][0] + result[0][0] > 9:
+    #         result[i].insert(0, memory_1[0][0])
+    #
+    #     if len(result[i]) < len(memory_1[i]):
+    #         result[i].insert(0, 0)
+
+
+
 
     print(memory_1)
     print(result)
 
-    # for i in range(len(result)):
-    #     for j in range(len(result[0])):
-    #         s = result[i][j] + memory_1[i][j]
-    #         fresult[i].append(s)
-    #
-    #
-    #
-    # matrix = fresult
-    # print(matrix)
-    # row_len = len(matrix)
-    # print(row_len)
-    # col_len = len(matrix[0])
-    # print(col_len)
-    # arr = []
-    # if row_len > 1:
-    #     for k in range(col_len - 1, 0, -1):
-    #         i = 0
-    #         j = k
-    #         sum1 = 0
-    #         while j <= col_len - 1:
-    #             sum1 += matrix[i][j]
-    #             i += 1
-    #             j += 1
-    #         arr.insert(0, sum1)
-    #
-    #     for k in range(len(matrix)):
-    #         i = k
-    #         j = 0
-    #         sum2 = 0
-    #         while i <= row_len - 1:
-    #             sum2 += matrix[i][j]
-    #             i += 1
-    #             j += 1
-    #         arr.insert(0, sum2)
-    #
-    #
-    # print(matrix)
-    # final_result = []
-    # memory_2 = []
-    # for k in reversed(arr):
-    #     if k > 9:
-    #         memo, k = int(str(k)[:-1]), int(str(k)[-1])
-    #         final_result.insert(0, k)
-    #         memory_2.append(memo)
-    #         print(memory_1)
-    #         print(result)
-    #
-    #     else:
-    #         final_result.insert(0, k)
-    #         if len(result) > len(memory_1):
-    #             memory_2.insert(0, 0)
-    #
-    # return ''.join([str(i) for i in final_result])
-    #
-    # return ''.join([str(i) for i in fresult[0]])
 
-
-x = 12345
-y = 12
+x = 99
+y = 99
 
 print(grade_mult(x, y))
 
