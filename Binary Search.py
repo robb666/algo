e069044ba1da16b6e6c6276ef2b6cd5b5de45cd0
robb666 <56496611+robb666@users.noreply.github.com>
2021-08-01 18:36:52 +0200

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -6,6 +6,61 @@
 #     return False
 
 
+def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
+    if right_index < left_index:
+        return 'not found'
+
+    mid_index = (left_index + right_index) // 2
+    mid_number = numbers_list[mid_index]
+
+    if mid_number == number_to_find:
+        return mid_index
+    if mid_number < number_to_find:
+        left_index = mid_index + 1
+    else:
+        right_index = mid_index - 1
+
+    return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
+
+
+if __name__ == '__main__':
+    numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
+    number_to_find = 9889
+
+    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list) - 1)
+    print(f'Number found at index {index} using bi search.')
+
+
+
+
+
+
+
+
+"""Binary Search 2"""
+def binary_search(numbers_list, number_to_find):
+    left_index = 0
+    right_index = len(numbers_list) - 1
+    mid_index = 0
+
+    while left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = numbers_list[mid_index]
+
+        if mid_number == number_to_find:
+            return mid_index
+
+        if mid_number < number_to_find:
+            left_index = mid_index + 1
+        else:
+            right_index = mid_index - 1
+
+    return False
+
+
+
+
+"""Binary Search 1"""
 def binary_search(numbers_list, number_to_find):
     left_index = 0
     right_index = len(numbers_list)
@@ -24,26 +79,11 @@ def binary_search(numbers_list, number_to_find):
     return False
 
 
-if __name__ == '__main__':
-    numbers_list = [12, 15, 17, 19, 21, 24, 45, 67, 88]
-    number_to_find = 14
-
-    index = binary_search(numbers_list, number_to_find)
-
-    isnot = 'not ' if not index else ''
-    index = index + ' ' if index else ''
-    print(f'Number {isnot}found at index {index}using bi search.')
-
-
-
-
-
-
-
-
-
-
-
+# numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
+# number_to_find = 19
+#
+# index = binary_search(numbers_list, number_to_find)
+# print(index)
 
 
 
