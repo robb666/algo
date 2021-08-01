29a255f091be3c1cc14d22c954c2ba80526ac550
robb666 <56496611+robb666@users.noreply.github.com>
2021-08-01 22:57:03 +0200

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -6,39 +6,56 @@
 #     return False
 
 
-def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
-    omo = []
-    if right_index < left_index:
-        return 'not found'
+def binary_search(numbers_list, number_to_find):
+    left_index = 0
+    right_index = len(numbers_list) - 1
+    mid_index = 0
 
-    mid_index = (left_index + right_index) // 2
-    mid_number = numbers_list[mid_index]
+    while left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = numbers_list[mid_index]
 
-    if mid_number == number_to_find:
-        i = 1
-        omo.append(mid_index)
-        while mid_index + i <= len(numbers_list) - 1 \
-                and (numbers_list[mid_index - i] == number_to_find or numbers_list[mid_index + i] == number_to_find):
-            if numbers_list[mid_index - i] == number_to_find:
-                omo.append(mid_index - i)
-            if numbers_list[mid_index + i] == number_to_find:
-                omo.append(mid_index + i)
-            i += 1
-        return sorted(omo)
-    if mid_number < number_to_find:
-        left_index = mid_index + 1
-    else:
-        right_index = mid_index - 1
-    return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
+        if mid_number == number_to_find:
+            return mid_index
 
+        if mid_number < number_to_find: # this means number is in right hand side of the list
+            left_index = mid_index + 1
+        else: # number to find is on left hand side of the list
+            right_index = mid_index - 1
+
+    return -1
+
+
+def find_all_occurences(numbers, number_to_find):
+    index = binary_search(numbers, number_to_find)
+    indices = [index]
+    # find indices on left hand site
+    i = index - 1
+    while i >= 0:
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i -= 1
+
+    # find indices on the right hand site
+    i = index + 1
+    while i < len(numbers):
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i += 1
+
+    return sorted(indices)
 
 
 if __name__ == '__main__':
     # numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
-    numbers_list = [1,4,6,9,11,15,15,17,21,34,34,56,56]
-    number_to_find = 56
+    numbers_list = [1,4,6,9,11,15,15,15,17,21,34,34,56,56]
+    number_to_find = 15
 
-    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list) - 1)
+    index = find_all_occurences(numbers_list, number_to_find)
 
     print(f'Number found at index {index} using bi search.')
 
