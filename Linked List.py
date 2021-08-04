b0dfa5fe226e903c2b1f5e6c4f4566640a814447
robb666 <robert.patryk.grzelak@gmail.com>
2021-08-04 15:28:48 +0200

    Update Linked List.py

--- Linked List.py
+++ Linked List.py
@@ -5,6 +5,7 @@ class Node:
         self.next = next
         self.prev = prev
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -12,19 +13,6 @@ class LinkedList:
     def insert_at_beginning(self, data):
         node = Node(data, self.head)
         self.head = node
-
-    def print(self):
-        if self.head is None:
-            print('Linked list is empty.')
-            return
-        itr = self.head
-        llstr = ''
-
-        while itr:
-            llstr += str(itr.data) + '-->'
-            itr = itr.next
-
-        print(llstr)
 
     def insert_at_end(self, data):
         if self.head is None:
@@ -95,51 +83,65 @@ class LinkedList:
             itr = itr.next
 
     def remove_by_value(self, data):
+        if self.head is None:
+            return
+
         itr = self.head
-        while itr:
-            print(itr.data, data)
-            if itr.data == data:
-                # itr.data = ''
+        if itr.data == data:
+            itr = self.head.next
 
+        while itr.next:
+            if itr.next.data == data:
                 itr.next = itr.next.next
-
                 break
 
             itr = itr.next
 
+    def print(self):
+        if self.head is None:
+            print('Linked list is empty.')
+            return
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+
+        print(llstr)
+
+    def print_forward(self):
+        if self.head is None:
+            print('Linked list is empty.')
+            return
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + ', '
+            itr = itr.next
+        print(llstr)
+
+    def print_backward(self):
+        pass
+
+
+
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insert_values(['banana', 'mango', 'grapes', 'orange'])
-    # ll.print()
-    ll.insert_after_value('mango', 'apple')
+    ll.insert_values(["banana","mango","grapes","orange"])
     ll.print()
-    ll.remove_by_value('apple')
+    ll.insert_after_value("mango", "apple") # insert apple after mango
     ll.print()
-    # ll.remove_by_value(0, 'figs')
-    # ll.print()
+    ll.remove_by_value("orange") # remove orange from linked list
+    ll.print()
+    # ll.remove_by_value("figs")
+    ll.print_forward()
     # ll.remove_by_value("banana")
     # ll.remove_by_value("mango")
     # ll.remove_by_value("apple")
     # ll.remove_by_value("grapes")
     # ll.print()
-
-    # ll.insert_at(2, 'jackfruit')
-    # ll.print()
-    # ll.rem0ve_at(2)
-    # print(ll.get_lenght())
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
