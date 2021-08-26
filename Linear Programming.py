9c40e9c58d4f108979e5a10da59d278a88344aa0
robb666 <56496611+robb666@users.noreply.github.com>
2021-08-26 08:11:47 +0200

    Update Linear Programming.py

--- Linear Programming.py
+++ Linear Programming.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 model = LpProblem(name="small-problem", sense=LpMaximize)
 
 # Initialize the decision variables
-x = LpVariable(name='x', lowBound=0)
+x = LpVariable(name='x', lowBound=0, cat='Integer')
 y = LpVariable(name='y', lowBound=0)
 
 
@@ -27,5 +27,18 @@ model += (-x + 5 * y == 15, 'green_constraints')
 
 
 
+model += lpSum([x, 2 * y])
+
+# Solve the problem
+status = model.solve()
+
+print(f"status: {model.status}, {LpStatus[model.status]}")
+
+
+print(f"objective: {model.objective.value()}")
+
+
+for var in model.variables():
+    print(f"{var.name}: {var.value()}")
 
 
