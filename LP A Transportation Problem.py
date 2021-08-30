26fb063957f970d678d3e10f8a48322094a2d354
robb666 <robert.patryk.grzelak@gmail.com>
2021-08-30 17:07:29 +0200

    update

--- LP A Transportation Problem.py
+++ LP A Transportation Problem.py
@@ -10,7 +10,8 @@ Authors: Antony Phillips, Dr Stuart Mitchell  2007
 Warehouses = ['A', 'B']
 
 # Creates a dictionary for the number of units of supply for each supply node
-supply = {'A': 1000, 'B': 4000}
+supply = {'A': 1000,
+          'B': 4000}
 
 # Creates a list of all demand nodes
 Bars = ['1', '2', '3', '4', '5']
@@ -32,23 +33,44 @@ costs = [  # Bars
 ]
 
 # The cost data is made into a dictionary
-costs_dict = {
-    'A': costs[0],
-    'B': costs[1],
-}
+costs = makeDict([Warehouses, Bars], costs, 0)
+print()
+print(costs)
+# Creates the 'prob' variable to contain the problem data
+prob = LpProblem('BeerDistributionProblem', LpMinimize)
 
-print(costs_dict['A'][0])
+# Creates a list of tuples containing all the possible routes for transport
+Routes = [(w, b) for w in Warehouses for b in Bars]
 
-# prob = LpProblem('TransportationProblem', sense=LpMinimize)
+# A dictionary called 'Vars' is created to contain the referenced variables(the routes)
+vars = LpVariable.dicts('Route', (Warehouses, Bars), 0, None, LpInteger)
+print()
+print(vars)
+# print(vars)
+# The objective function is added to 'prob' first
+prob += lpSum([vars[w][b] * costs[w][b]] for w, b in Routes), 'Sum_of_Transporting_Costs'
 
-# all_nodes = Warehouses + Bars
+# The supply maximum constraints are added to 'prob' for each supply node
+for w in Warehouses:
+    prob += lpSum([vars[w][b] * costs[w][b] for b in Bars]) <= supply[w], 'Sum_of_Products_out_of_Warehouse_%s' % w
 
-print(Warehouses.extend(Bars))
+# The demand minimum constraints are added to prob for each demand node (bar)
+for b in Bars:
+    prob += lpSum([vars[w][b] * costs[w][b] for w in Warehouses]) >= demand[b], 'Sum_of_products_into_Bars%s' % b
 
-# all_nodes_dict = LpVariable.dict('all_nodes', all_nodes)
-
-
-
+# prob.writeLP('BeerDistributionProblem.lp')
+#
+# prob.solve()
+#
+# # The status of the solution is printed to the screen
+# print('Status: ', LpStatus[prob.status])
+#
+# # Each of the variables is printed with it's resolved optimum value
+# for v in prob.variables():
+#     print(f'{v.name}, = {v.varValue}')
+#
+# # The optimised objective function value is printed to the screen
+# print("Total Cost of Transportation = ", value(prob.objective))
 
 
 
