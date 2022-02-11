fbf4ac224f18959c90fc576bf8ab072df3a84640
Robb <56496611+robb666@users.noreply.github.com>
2022-02-11 09:47:01 +0100

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,12 +1,30 @@
 
 
-marked = [False] * G.size()
 
 
 def dfs(G, v):
-    visit(v)
+    # visit(v)
     marked[v] = True
-    for w in G.neighbours(v):
-        if not marked[w]:
-            dfs(G, w)
+    for w in G.keys():
+        if not marked[int(w)]:
+            dfs(G, int(w))
+    return marked
+
+
+if __name__ == '__main__':
+
+    graph = {'0': {'1', '2'},
+             '1': {'0', '3', '4'},
+             '2': {'0'},
+             '3': {'1'},
+             '4': {'2', '3'},
+             '5': {'4'}}
+
+    print(len(graph))
+
+    marked = [False] * len(graph)
+
+    print(marked)
+
+    print(dfs(graph, 0))
 
