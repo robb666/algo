160e73d0ea52cd9964a044efa82ea4db9c1b22a5
robb666 <56496611+robb666@users.noreply.github.com>
2022-02-13 23:04:23 +0100

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,31 +1,47 @@
+import logging
 
 
+# def dfs_rec(G, v, visited):
+#
+#     visited.append(v)
+#     for v in G[v]:
+#         dfs(G, v, visited)
+#
+#     return visited
 
 
-def dfs(G, v):
-    visited = set()
-    marked[v] = True
-    for w in G:
-        if not marked[int(w)]:
-            dfs(G, int(w))
-        visited.add(w)
+def dfs_it(G, v):
+
+    visited = []
+
+    for v in range(len(G)):
+        if v:
+            value = G[v].pop(0)
+            print(G[value])
+
+            visited.append(G[value])
+        # else:
+
+
     return visited
 
 
 if __name__ == '__main__':
 
-    graph = {'0': {'1', '2'},
-             '1': {'0', '3', '4'},
-             '2': {'0'},
-             '3': {'1'},
-             '4': {'2', '3'},
-             '5': {'4'}}
+    # graph = {'0': {'1', '2'},
+    #          '1': {'0', '3', '4', '2'},
+    #          '2': {'0'},
+    #          '3': {'1'},
+    #          '4': {'2', '3'},
+    #          '5': {'4'}}
 
-    print(len(graph))
-
-    marked = [False] * len(graph)
-
-    print(marked)
-
-    print(dfs(graph, 0))
+    graph = {'a': ['b', 'c'],
+             'b': ['d'],
+             'c': ['e'],
+             'd': ['f'],
+             'e': [],
+             'f': []}
+    # marked = [False] * len(graph)
+    # print(dfs_rec(graph, 'a', []))
+    print(dfs_it(graph, 'a'))
 
