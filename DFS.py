329c511f335b0557c670262c77669da8e87d4112
robb666 <56496611+robb666@users.noreply.github.com>
2022-02-19 21:17:37 +0100

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -12,19 +12,31 @@ import logging
 
 def dfs_it(G, v):
 
-    visited = []
-    visited.append(v)
+    values = []
+    values.append(v)
+    # while G.values():
+    for arr in G.values():
+        # values.append(arr)
+        while arr:
+            # for _ in arr:
+                try:
+                    v0 = arr.pop(0)
+                    values.append(v0)
 
-    for node in G:
-        neighbours = G[node]
-        if neighbours:
-            next = neighbours.pop(0)
-            current = G[next]
-            visited.append(current)
+                    v1 = G[v0].pop(0)
+                    values.append(v1)
 
+                    v2 = G[v1].pop(0)
+                    values.append(v2)
 
+                    v3 = G[v2].pop(0)
+                    values.append(v3)
+                except:
 
-    return visited
+                    # print(values)
+                    pass
+
+    return values
 
 
 if __name__ == '__main__':
@@ -32,13 +44,33 @@ if __name__ == '__main__':
     graph = {'a': ['b', 'c'],
              'b': ['d'],
              'c': ['e'],
-             'd': ['f'],
+             'd': ['f', 'g'],
              'e': [],
-             'f': []}
+             'f': [],
+             'g': []}
     # marked = [False] * len(graph)
     # print(dfs_rec(graph, 'a', []))
     print(dfs_it(graph, 'a'))
 
+
+
+# graph = {'a': ['b', 'c'],
+#          'b': ['d'],
+#          'c': ['e'],
+#          'd': ['f'],
+#          'e': [],
+#          'f': []}
+#
+#
+# while graph:
+#     for v in graph.values():
+#         if v:
+#             v.pop(0)
+#
+#
+#
+# for v in G values,pop(0) if G[values.pop(0)]
+#     if v.pop(0) empty then for v in G.pop(0)
 
 
 # def dfs_it(G, v):
