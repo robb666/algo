58d0b4430defdbe6a4108598e2aa6acd1b5f9202
robb666 <56496611+robb666@users.noreply.github.com>
2022-02-20 14:11:29 +0100

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,28 +1,25 @@
-import logging
+
+# def dfs_rec(G, v, visited):
+#
+#     visited.append(v)
+#     for v in G[v]:
+#         dfs_rec(G, v, visited)
+#
+#     return visited
 
 
-def dfs_rec(G, v, visited):
+def dfs_it(G, v):
 
-    visited.append(v)
-    for v in G[v]:
-        dfs_rec(G, v, visited)
+    stack = [v]
+    visited = []
+
+    while len(stack) > 0:
+        current = stack.pop()
+        visited.append(current)
+        for neighbour in G[current][::-1]:
+            stack.append(neighbour)
 
     return visited
-
-
-
-
-# def dfs_it(G, v):
-#
-#     stack = [v]
-#
-#     while len(stack) > 0:
-#         current = stack.pop()
-#         print(current)
-#         for neighbour in G[current][::-1]:
-#             stack.append(neighbour)
-#
-#     return stack
 
 
 if __name__ == '__main__':
@@ -35,33 +32,4 @@ if __name__ == '__main__':
              'g': []
              }
 
-    print(dfs_rec(graph, 'a', []))
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def dfs_it(G, v):
-#
-#     visited = []
-#     visited.append(v)
-#
-#     for k in G[v]:
-#         for v in k:
-#             visited.append(v)
-#             if G[v]:
-#                 for n in G[v]:
-#                     visited.append(n)
-#                     if G[n]:
-#                         current = G[n].pop(0)
-#                         visited.append(current)  # f
-#
-#     return visited
+    print(dfs_it(graph, 'a'))
