6aab81d914a7223cac6a0f33c3884e681e18f0fa
robb666 <56496611+robb666@users.noreply.github.com>
2022-03-01 23:12:14 +0100

    up

--- Maze problem/generic_search.py
+++ Maze problem/generic_search.py
@@ -24,6 +24,24 @@ class Stack(Generic[T]):
         return repr(self._container)
 
 
+class Queue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: Deque[T] = Deque()
+
+    @property
+    def empty(self) -> bool:
+        return not self._container  # not is true for empty container
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.popleft()  # FIFO
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
 class Node(Generic[T]):
     def __init__(self, state: T, parent: Optional[Node],
                  cost: float = 0.0, heuristic: float = 0.0) -> None:
@@ -59,6 +77,29 @@ def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], Li
     return None  # went through everything and never found goal
 
 
+def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    # frontier is where we've yet to go
+    frontier: Queue[Node[T]] = Queue()
+    frontier.push(Node(initial, None))
+    # explored is where we have been
+    explored: Set[T] = {initial}
+
+    # keep going while is more to explore
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # if we found the goal, we've done
+        if goal_test(current_state):
+            return current_node
+        # if we check where we can go next and haven't explored
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None  # went through everything and never found goal
+
+
 def node_to_path(node: Node[T]) -> List[T]:
     path: List[T] = [node.state]
     # work backwards from end to front
@@ -69,19 +110,25 @@ def node_to_path(node: Node[T]) -> List[T]:
     return path
 
 
-# class Queue(Generic[T]):
-#     def __init__(self) -> None:
-#         self._container: Deque[T] = Deque()
-#
-#     @property
-#     def empty(self) -> bool:
-#         return not self._container  # not is true for empty container
-#
-#     def push(self, item: T) -> None:
-#         self._container.append(item)
-#
-#     def pop(self) -> T:
-#         return self._container.popleft()  # FIFO
-#
-#     def __repr__(self) -> str:
-#         return repr(self._container)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Maze problem/maze.py
+++ Maze problem/maze.py
@@ -3,7 +3,7 @@ from typing import List, NamedTuple, Callable, Optional
 import random
 from math import sqrt
 # from generic_search import dfs, bfs, node_to_path, astar, Node
-from generic_search import dfs, node_to_path, Node
+from generic_search import dfs, bfs, node_to_path, Node
 
 
 class Cell(str, Enum):
@@ -107,16 +107,16 @@ if __name__ == '__main__':
         print(m)
         m.clear(path1)
 
-    # # Test BFS
-    # solution2: Optional[Node[Maze]] = bfs(m.start, m.goal_test, m.successors)
-    # if solution2 is None:
-    #     print('No solution found using breadth-first search!')
-    # else:
-    #     path2: List[MazeLocation] = node_to_path(solution2)
-    #     m.mark(path2)
-    #     print(m)
-    #     m.clear(path2)
-    #
+    # Test BFS
+    solution2: Optional[Node[Maze]] = bfs(m.start, m.goal_test, m.successors)
+    if solution2 is None:
+        print('\nNo solution found using breadth-first search!')
+    else:
+        path2: List[MazeLocation] = node_to_path(solution2)
+        m.mark(path2)
+        print(m)
+        m.clear(path2)
+
     # # Test A*
     # distance: Callable[[MazeLocation], float] = manhattan_distance(m.goal)
     # solution2: Optional[Node[MazeLocation]] = astar(m.start, m.goal_test, m.successors, distance)
