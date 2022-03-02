8e04f789f364f4c2231214c877b92d235bd4d508
Robb <56496611+robb666@users.noreply.github.com>
2022-03-02 10:55:09 +0100

    update

--- Maze problem/generic_search.py
+++ Maze problem/generic_search.py
@@ -42,6 +42,24 @@ class Queue(Generic[T]):
         return repr(self._container)
 
 
+class PriorityQueue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container  # not is true for empty container
+
+    def push(self, item: T) -> None:
+        heappush(self._container, item)  # in by priority
+
+    def pop(self) -> T:
+        return heappop(self._container)  # out by priority
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
 class Node(Generic[T]):
     def __init__(self, state: T, parent: Optional[Node],
                  cost: float = 0.0, heuristic: float = 0.0) -> None:
@@ -98,6 +116,32 @@ def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], Li
             explored.add(child)
             frontier.push(Node(child, current_node))
     return None  # went through everything and never found goal
+
+
+def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]], heuristic: Callable[[T], float]) -> Optional[Node[T]]:
+    # frontier is where we have yet to go
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    frontier.push(Node(initial, None, 0.0, heuristic(initial)))
+    # explored is where we have been
+    explored: Dict[T, float] = {initial: 0.0}
+
+    # keep going while is more to explore
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # if we found the goal, we are done
+        if goal_test(current_state):
+            return current_node
+        # check where we can go next and have not explored
+        for child in successors(current_state):
+            new_cost: float = current_node.cost + 1  # 1 assumes a grid, need a cost function for more sophisticated..
+
+            if child not in explored or explored[child] > new_cost:
+                explored[child] = new_cost
+                frontier.push(Node(child, current_node, new_cost, heuristic(child)))
+
+    return None  # went through everything and never found goal
+
 
 
 def node_to_path(node: Node[T]) -> List[T]:
--- Maze problem/maze.py
+++ Maze problem/maze.py
@@ -2,8 +2,7 @@ from enum import Enum
 from typing import List, NamedTuple, Callable, Optional
 import random
 from math import sqrt
-# from generic_search import dfs, bfs, node_to_path, astar, Node
-from generic_search import dfs, bfs, node_to_path, Node
+from generic_search import dfs, bfs, node_to_path, astar, Node
 
 
 class Cell(str, Enum):
@@ -108,7 +107,7 @@ if __name__ == '__main__':
         m.clear(path1)
 
     # Test BFS
-    solution2: Optional[Node[Maze]] = bfs(m.start, m.goal_test, m.successors)
+    solution2: Optional[Node[MazeLocation]] = bfs(m.start, m.goal_test, m.successors)
     if solution2 is None:
         print('\nNo solution found using breadth-first search!')
     else:
@@ -117,12 +116,12 @@ if __name__ == '__main__':
         print(m)
         m.clear(path2)
 
-    # # Test A*
-    # distance: Callable[[MazeLocation], float] = manhattan_distance(m.goal)
-    # solution2: Optional[Node[MazeLocation]] = astar(m.start, m.goal_test, m.successors, distance)
-    # if solution2 is None:
-    #     print('No solution found using A*!')
-    # else:
-    #     path3: List[MazeLocation] = node_to_path(solution3)
-    #     m.mark(path3)
-    #     print(m)
+    # Test A*
+    distance: Callable[[MazeLocation], float] = manhattan_distance(m.goal)
+    solution3: Optional[Node[MazeLocation]] = astar(m.start, m.goal_test, m.successors, distance)
+    if solution2 is None:
+        print('No solution found using A*!')
+    else:
+        path3: List[MazeLocation] = node_to_path(solution3)
+        m.mark(path3)
+        print(m)
