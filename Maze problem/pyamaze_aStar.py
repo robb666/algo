a5d1f549cc6a5b06f20636f26906af0f77620e0c
robb666 <56496611+robb666@users.noreply.github.com>
2022-03-13 23:33:57 +0100

    Update pyamaze_aStar.py

--- Maze problem/pyamaze_aStar.py
+++ Maze problem/pyamaze_aStar.py
@@ -1,4 +1,4 @@
-from pyamaze import maze
+from pyamaze import maze, agent, textLabel
 from queue import PriorityQueue
 
 
@@ -11,48 +11,54 @@ def h(cell1, cell2):
 
 def aStar(m):
     start = (m.rows, m.cols)
-    g_score = {cell:float('inf') for cell in m.grid}
+    g_score = {cell: float('inf') for cell in m.grid}
     g_score[start] = 0
-    f_score = {cell:float('inf') for cell in m.grid}
+    f_score = {cell: float('inf') for cell in m.grid}
     f_score[start] = h(start, (1, 1))
 
     open = PriorityQueue()
     open.put((h(start, (1, 1)), h(start, (1, 1)), start))
+    aPath = {}
 
     while not open.empty():
         currCell = open.get()[2]
-        ...
+        if currCell == (1, 1):
+            break
+        for d in 'ESNW':
+            if m.maze_map[currCell][d]:
+                if d == 'E':
+                    childCell = (currCell[0], currCell[1] + 1)
+                if d == 'W':
+                    childCell = (currCell[0], currCell[1] - 1)
+                if d == 'N':
+                    childCell = (currCell[0] - 1, currCell[1])
+                if d == 'S':
+                    childCell = (currCell[0] + 1, currCell[1])
+
+                temp_g_score = g_score[currCell] + 1
+                temp_f_score = temp_g_score + h(childCell, (1, 1))
+
+                if temp_f_score < f_score[childCell]:
+                    g_score[childCell] = temp_g_score
+                    f_score[childCell] = temp_f_score
+                    open.put((temp_f_score, h(childCell, (1, 1)), childCell))
+                    aPath[childCell] = currCell
+
+    fwdPath = {}
+    cell = (1, 1)
+    while cell != start:
+        fwdPath[aPath[cell]] = cell
+        cell = aPath[cell]
+    return fwdPath
 
 
+if __name__ == '__main__':
+    m = maze(5, 5)
+    m.CreateMaze()
+    path = aStar(m)
 
-m = maze(5, 5)
-m.CreateMaze()
+    a = agent(m, footprints=True)
+    m.tracePath({a: path})
+    l = textLabel(m, 'A Star Path Lenght', len(path))
 
-
-
-m.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    m.run()
