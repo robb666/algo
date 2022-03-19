66d0e9a7658731d5ffda907f9a4ff6608d452fe8
robb666 <56496611+robb666@users.noreply.github.com>
2022-03-19 13:53:15 +0100

    up

--- Selenium ai/practice_forests.py
+++ Selenium ai/practice_forests.py
@@ -30,14 +30,9 @@ url = r'https://portal.generali.pl/auth/login?service=https%3A%2F%2Fportal.gener
 driver = webdriver.Chrome()
 url1 = 'https://accounts.lambdatest.com/login'
 
-attr_tag = ['input', 'button']
-attr_li = ['ELEMENT', 'TAG', 'ID', 'TYPE', 'CLASS', 'NAME', 'ARIA_AUTOCOMPLETE', 'TITLE', 'HREF', 'TEXT', 'VALUE', 'ARIA_LABEL']
-elements_arr = ['email', 'password', 'LOGIN', 'checkbox']
-
-# d = dict(attr_li=np.array(attr_li),
-#          attr_tag=np.array(attr_tag),
-#          elements_arr=np.array(elements_arr),
-#          )
+tags = ['input', 'button']
+attr_li = ['element', 'tag', 'id', 'type', 'class', 'name', 'aria_autocomplete', 'title', 'href', 'text', 'value', 'aria_label']
+ele_arr = ['Email', 'Password', 'LOGIN', 'Checkbox']
 
 
 driver.get(url1)
@@ -45,63 +40,87 @@ html = driver.page_source
 
 soup = BeautifulSoup(html, 'lxml')
 
-# df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in d.items()]))
-tag = 'button'
+arr = []
+n = 0
+for tag in tags:
+    for attr in soup.find_all(tag):
 
-attr = soup.find_all(tag)
+        get_class = attr.get('class')[0] if len(attr.get('class')) > 0 else None
+        arr.append((ele_arr[n], tag, attr.get('id'), attr.get('type'), get_class, attr.get('name'), attr.get('aria_autocomplete'),
+                    attr.get('title'), attr.get('href'), attr.text, attr.get('value'), attr.get('aria_label')))
+        n += 1
 
-atributes = attr[0].attrs
+df = pd.DataFrame(arr)
+df.columns = attr_li
 
-print(atributes)
+print(df)
+df.to_csv('file.csv')
 
-
-
-
-
-# id_ = elements[0].attrs
-# print(id_)
-
-# for tag in attr_tag:
-# for attr in soup.find_all('button'):
-    # print(dict)
-    # print(attr.attrs)
-    # # print(tag)
-    # print(attr.attrs.get('id'))
-    # print(attr.id)
-    # print(attr.attrs.get('type'))
-    # print(attr.attrs.get('class'))
-    # print(attr.attrs.get('name'))
-    # print(attr.attrs.get('aria-autocomplete'))
-    # print(attr.attrs.get('title'))
-    # print(attr.attrs.get('href'))
-    # print('text:')
-    # print(attr.text)
-    # print(attr.value)
-    # print(attr.attrs.get('value'))
-    # print(attr.attrs.get('aria-label'))
-    # print('\n\n')
-    # print(attr.text)
-    # print(attr.value)
-    # print(attr.href)
-    # print(attr.contents)
+Test = df.loc[(df['element'] == 'Password')]
+Test.to_csv('Test.csv')
 
 
-# df = pd.DataFrame({'ELEMENT': elements_arr,
-#                    # 'TAG': tag,
-#                    'ID': attr.attrs.get('id'),
-#                    'TYPE': attr.attrs.get('type'),
-#                    'CLASS': '2',
-#                    'NAME': attr.attrs.get('name'),
-#                    'ARIA_AUTOCOMPLETE': attr.attrs.get('aria-autocomplete'),
-#                    'TITLE': attr.attrs.get('title'),
-#                    'HREF': attr.attrs.get('href'),
-#                    'TEXT': '',
-#                    'VALUE': attr.attrs.get('value'),
-#                    # 'ARIA_LABEL': '',
-#                    'PLACEHOLDER': attr.attrs.get('placeholder'),
-#                    })
-#
-#
-# print(df)
+driver.quit()
 
+
+
+
+
+
+
+
+        # df = pd.DataFrame({'ELEMENT': elements_arr,
+        #                    'TAG': tag,
+        #                    'ID': attr.get('id'),
+        #                    'TYPE': attr.get('type'),
+        #                    'CLASS': get_class,
+        #                    'NAME': attr.get('name'),
+        #                    'ARIA_AUTOCOMPLETE': attr.get('aria-autocomplete'),
+        #                    'TITLE': attr.get('title'),
+        #                    'HREF': attr.get('href'),
+        #                    'TEXT': attr.get('text'),
+        #                    'VALUE': attr.get('value'),
+        #                    'ARIA_LABEL': '',
+        #                    'PLACEHOLDER': attr.get('placeholder'),
+        #                    })
+
+
+
+
+
+
+
+
+
+
+
+
+    # print(df)
+
+        # id_ = elements[0].attrs
+        # print(id_)
+
+        # for tag in attr_tag:
+        # for attr in soup.find_all('button'):
+        # print(dict)
+        # print(attr.attrs)
+        # # print(tag)
+        # print(attr.attrs.get('id'))
+        # print(attr.id)
+        # print(attr.attrs.get('type'))
+        # print(attr.attrs.get('class'))
+        # print(attr.attrs.get('name'))
+        # print(attr.attrs.get('aria-autocomplete'))
+        # print(attr.attrs.get('title'))
+        # print(attr.attrs.get('href'))
+        # print('text:')
+        # print(attr.text)
+        # print(attr.value)
+        # print(attr.attrs.get('value'))
+        # print(attr.attrs.get('aria-label'))
+        # print('\n\n')
+        # print(attr.text)
+        # print(attr.value)
+        # print(attr.href)
+        # print(attr.contents)
 
