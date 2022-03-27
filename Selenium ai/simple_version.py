ee3953770120be1b884ed38af88de56408fd2669
robb666 <56496611+robb666@users.noreply.github.com>
2022-03-27 11:32:05 +0200

    Update simple_version.py

--- Selenium ai/simple_version.py
+++ Selenium ai/simple_version.py
@@ -19,31 +19,35 @@ url = r'https://portal.generali.pl/auth/login?service=https%3A%2F%2Fportal.gener
 
 driver.get(url)
 
-driver.maximize_window()
 
+def no_such_element(*, e, row, attr, value):
+    """Useful when attribute changes name but not order within tags.
+       This function can send_keys or click to an element.
+       The 'attr' is column of the DataFrame so simply specify
+       row by index and column by name of the element.
+       If value=False then function will send a click."""
 
-def no_such_element(e):
     if 'no such element' in str(e) or 'Unable to locate element' in str(e):
         html = driver.page_source
         soup = BeautifulSoup(html, 'lxml')
-        tags = ['input']  # tu uzupełniać..
+        tags = ['input']  # Fill in tags.
 
         arr = []
         for tag in tags:
             for element in soup.find_all(tag):
-                arr.append({'tag': tag} |
-                           {'text': element.text} |
-                           element.attrs)
+                arr.append({'tag': tag} | {'text': element.text} | element.attrs)
 
         df = pd.DataFrame.from_records(arr)
-
         df.insert(0, 'element', df.name)
         df.element.fillna(df['value'], inplace=True)
         df.element.fillna(df['text'], inplace=True)
-        print(df)
-
-        element = df.iloc[0]['name']
-        driver.find_element(By.XPATH, f"//*[@name='{element}']").send_keys(login)
+        # print(df)
+        el_attr = df.iloc[row][attr]
+        selector = driver.find_element(By.XPATH, f"//*[@{attr}='{el_attr}']")
+        if value:
+            selector.send_keys(value)
+        else:  # click
+            selector.click()
 
 
 try:
@@ -51,5 +55,4 @@ try:
     Element_email.send_keys('ubezpieczenia.magro@gmail.com')
 
 except Exception as e:
-    no_such_element(e)
-    time.sleep(6)
+    no_such_element(e=e, row=0, attr='name', value='ubezpieczenia.magro@gmail.com')
