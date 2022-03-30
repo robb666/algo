c99a834b72762b603d975ddd0bd3e2728f7f939a
robb666 <56496611+robb666@users.noreply.github.com>
2022-03-30 23:46:35 +0200

    Update rf_trials.py

--- Selenium ai/rf_trials.py
+++ Selenium ai/rf_trials.py
@@ -8,14 +8,74 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
 
-
 df = pd.read_csv('file.csv')
+
 test = pd.read_csv('Test.csv')
 
 df = df.fillna('None')
 
 print(df)
 print()
+
+dummies = pd.get_dummies(df['element'])
+
+print(dummies)
+
+merged = pd.concat([df, dummies], axis=1)
+
+print(merged)
+
+final = merged.drop(['element', 'password'], axis=1)
+
+print(final)
+
+X = final.drop(['name'], axis='columns')
+
+print(X)
+
+y = final.name
+
+print(y)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+oneh = OneHotEncoder(handle_unknown='ignore')
+
+
+dfle = df
+# dfle = le.fit_transform(dfle[])
+
+X = oneh.fit_transform(X).toarray()
+
+
+
+# print(X)
+
+twoh = OneHotEncoder(handle_unknown='ignore')
+twoh.fit_transform(y).toarray()
+
+
+
+
+# print(oneh.transform(X).toarray())
+
+
+
+# print(dfle)
+
+
+
+model = LinearRegression()
+
+# model.fit(X, y)
+
+
+
+
+
+
 
 
 
@@ -26,35 +86,43 @@ oneh = OneHotEncoder(handle_unknown='ignore')
 
 
 
-
-# X_train = df.drop('element', axis=1)
 X_train = df
-oneh.fit(X_train)
-# oneh.transform(X_train).toarray()
-print(oneh.transform(X_train).toarray())
-
-print()
+# print(oneh.transform(X_train).toarray())
 
 
 
-# # returns all the unique Elements stored in the training data
-# df['element'].unique()
-# # creating a dictionary of elements
-# element_dict = dict(zip(df['element'].unique(), range(df['element'].nunique())))
-# y_train = df['element'].replace(element_dict)
-
-
-y_train = test
-
-oneh.fit(y_train)
-print(oneh.transform(y_train).toarray())
-
-
-# print(train.fit_transform(train).toarray())
 
 
 
-# oneh.fit(y_train)
-# oneh.transform(y_train).toarray()
+
+# print()
+#
+#
+# y_train = test
+#
+# oneh.fit_transform(y_train)
 # print(oneh.transform(y_train).toarray())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
