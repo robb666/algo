b5aed79c6906d246c5f56d81ed1a8079051f6796
robb666 <56496611+robb666@users.noreply.github.com>
2022-04-18 10:45:19 +0200

    up

--- Selenium ai/ohe_comparision.py
+++ Selenium ai/ohe_comparision.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
-from queue import PriorityQueue
-import re
+
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
+
 
 def scrp(driver):
 
@@ -26,101 +27,44 @@ def scrp(driver):
 
     df = pd.DataFrame.from_records(arr)
     # el_name = pd.DataFrame({'element': el_name})
-    # df = pd.concat([el_name, df], axis=1)
+    # df = pd.concat([el_name, df], axis=0)
     df.insert(0, 'element', df.name)
     df.element.fillna(df['value'], inplace=True)
     df.element.fillna(df['text'], inplace=True)
     # df.text = np.nan  # fillna didn't work
 
     ########
-    df.to_csv('file.csv')
-    df = pd.read_csv('file.csv', dtype=object)
-    print(df.dtypes)
+    df.to_csv('gen.csv')
+    df = pd.read_csv('gen.csv', dtype=object)
+
     return df
 
 
-def new_locator(driver):
+def new_locator(driver, *, attr, element_row, value):
     df = scrp(driver)
     df = df.fillna('None')
-    # df = df.drop(['Unnamed: 0'], axis=1)
-    print(df)
+    df = df.drop(['Unnamed: 0'], axis=1)
 
     to_test = pd.read_csv('Test.csv', dtype=object,
                           header=0, usecols=lambda c: c in df.columns)
-    test = to_test.append(df)[df.columns].iloc[:1, :]
+    test = to_test.append(df)[df.columns].iloc[:element_row, :]
     test = test.fillna('None')
-    # test = test.drop(['Unnamed: 0'], axis=1)
-    print(test)
-
-
-
-
 
     ohe = OneHotEncoder(sparse=False, handle_unknown='ignore')
     X_train = ohe.fit_transform(df.astype(str))
-
     X_test = ohe.transform(test)
-    print(X_test[0])
 
     element_dict = dict(zip(df['element'].unique(), range(df['element'].nunique())))
-    print(element_dict)
     y_train = df['element'].replace(element_dict)
 
     rf = RandomForestClassifier(n_estimators=50, random_state=0)
     rf.fit(X_train, y_train)
 
     probabilities = rf.predict_proba(X_test)[0]
-    print(probabilities)
+    el_attr = list(element_dict.keys())[np.argmax(probabilities)]
 
-    new_element = list(element_dict.keys())[np.argmax(probabilities)]
-    print(new_element)
-
-    new_locator = f"//*[@name='{new_element}']"
-
-    return new_locator
-
-
-
-
-
-# d = {}
-# n = 0
-# for idx, i in enumerate(range(len(X_train))):
-#     for j in range(len(X_train[0])):
-#         if X_train[i][j] == X_test[0][j]:
-#             n += 1
-#             d[idx] = n
-#     n = 0
-#
-# print()
-#
-# print(d)
-# element_idx = max(d, key=d.get)
-#
-# new_element = df.iloc[element_idx]['element']
-#
-# new_locator = f"//*[@name='{new_element}']"
-#
-# print(new_locator)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    selector = driver.find_element(By.XPATH, f"//*[@{attr}='{el_attr}']")
+    if value:
+        selector.send_keys(value)
+    else:  # click
+        selector.click()
--- Selenium ai/practice_forests.py
+++ Selenium ai/practice_forests.py
@@ -23,97 +23,13 @@ url = r'https://portal.generali.pl/auth/login?service=https%3A%2F%2Fportal.gener
 
 driver.get(url)
 
-# html = driver.page_source
-# soup = BeautifulSoup(html, 'lxml')
-# tags = ['input']  # to uzupełniać..?
-# # el_name = ['LOGIN', 'PASSW', 'LOG_BUTTON']
-#
-# arr = []
-# # tst = []
-# for tag in tags:
-#     for element in soup.find_all(tag):
-#         arr.append({'tag': tag} |
-#                    {'text': element.text} |
-#                    element.attrs)
-#
-# df = pd.DataFrame.from_records(arr)
-# # el_name = pd.DataFrame({'element': el_name})
-# # df = pd.concat([el_name, df], axis=1)
-# df.insert(0, 'element', df.name)
-# df.element.fillna(df['value'], inplace=True)
-# df.element.fillna(df['text'], inplace=True)
-#
-# # print(df)
-# Test = df.loc[(df['element'] == 'username')]
-# print(Test)
-# Test.to_csv('Test.csv')
-# # Test.to_sql('Testdb')
-
-
-
 driver.maximize_window()
 
-
-locator = 'usernameeeee'
 try:
-    Element_email = driver.find_element(By.XPATH, f"//input[@name='{locator}']")
+    Element_email = driver.find_element(By.XPATH, f"//input[@name='username']")
     Element_email.send_keys('ubezpieczenia.magro@gmail.com')
-
 except Exception as e:
-    el_from_e = eval(re.search('({.+})', e.args[0]).group())
-    selector = el_from_e['selector']
-    print(selector)
-
-    new_locator = new_locator(driver)
-
-
-    # html = driver.page_source
-    # soup = BeautifulSoup(html, 'lxml')
-    # tags = ['input']  # to uzupełniać..?
-    # # el_name = ['LOGIN', 'PASSW', 'LOG_BUTTON']
-    #
-    # arr = []
-    # # tst = []
-    # for tag in tags:
-    #     for element in soup.find_all(tag):
-    #         arr.append({'tag': tag} |
-    #                    {'text': element.text} |
-    #                    element.attrs)
-    #
-    # df = pd.DataFrame.from_records(arr)
-    # # el_name = pd.DataFrame({'element': el_name})
-    # # df = pd.concat([el_name, df], axis=1)
-    # df.insert(0, 'element', df.name)
-    # df.element.fillna(df['value'], inplace=True)
-    # df.element.fillna(df['text'], inplace=True)
-    #
-    # df.to_csv('file.csv')
-    # print(df)
-
-    # from rand_forests_sel import predict_elements, get_predicted_element
-    # from queue import PriorityQueue
-
-    # scores, element_name, test_df = predict_elements()
-    # print(scores)
-    # print(element_name)
-    # print(test_df)
-    #
-    # queue = PriorityQueue()
-    #
-    # predicted_element = get_predicted_element(scores, queue)[1]
-    # print(predicted_element)
-    #
-    # # print(scores)
-    # # predicted_element = scores[0][0]
-    #
-    # new_locator = f"//*[@name='{predicted_element}']"
-
-
-    # driver.execute_script(f"document.getElementById('{new_element}').value='ubezpieczenia.magro@gmail.com'")
-    driver.find_element(By.XPATH, new_locator).send_keys(login)
-    #
-    print('wpisane!!!')
-    time.sleep(50)
+    new_locator(driver, attr='name', element_row=1, value='ubezpieczenia.magro@gmail.com')
 
 
 
@@ -124,10 +40,13 @@ except Exception as e:
 
 
 
-# driver.quit()
 
 
 
+
+
+
+# driver.execute_script(f"document.getElementById('{new_element}').value='ubezpieczenia.magro@gmail.com'")
 # body = driver.find_element(By.XPATH, "//body")
 # body_attr = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', body)
 # attr_li = ['class', 'div', 'id', 'input', 'name', 'type', 'value', 'span']
