f8ebd5f1e84eac8d9c78114e93c1c07253fe8af6
Robb <56496611+robb666@users.noreply.github.com>
2022-09-16 11:31:31 +0200

    Update VisualPatterns.org.py

--- VisualPatterns.org.py
+++ VisualPatterns.org.py
@@ -1,30 +1,56 @@
-
+# https://www.visualpatterns.org/
 
 
 
 def vis1(n):
-    result = ''
-    for i in range(n + 1):
-        result += 'O'
-
-    return result
+    return ('O' * n + '\n') * n
 
 
-print('\nvisual1' + '\n\n')
-for n in range(4):
-    print(vis1(n))
+print('\nvisual 1' + '\n\n')
+[print(vis1(n)) for n in range(4)]
+
+
+########################################
 
 
 
+def vis2(n):
+    return (' ' * (n - 1) + 'O' + '\n') * (n - 1) + 'O' * n
 
-def vis32(n):
-    result = ''
+
+print('\nvisual 2\n\n')
+[print(vis2(i) + '\n') for i in range(4)]
+
+
+
+########################################
+
+
+
+def vis3(n):
+    return ('O' * n + ' ' * n + '\n') * n  # TODO
+
+
+print('\nvisual 3\n\n')
+for i in range(5):
+    print(vis3(i))
+# [print(vis3(i)) for i in range(4)]
+
+
+
+
+
+########################################
+
+
+
+
+def vis32(n, result=''):
     for _ in range(n + 1):
         result += 'O' * n + '\n'
-
     return result
 
 
-print('\n' * 4 + 'visual32')
+print('\n' * 4 + 'visual 32')
 for n in range(4):
     print(vis32(n))
