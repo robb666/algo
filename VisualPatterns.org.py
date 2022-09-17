374eb21ede5155daba85c475f4f064ea6afc5e0a
robb666 <56496611+robb666@users.noreply.github.com>
2022-09-17 14:49:26 +0200

    Update VisualPatterns.org.py

--- VisualPatterns.org.py
+++ VisualPatterns.org.py
@@ -41,7 +41,7 @@ def vis4(n):
     result = ''
     for i in range(0, n + 1):
         result += ' ' * i + 'O' + ' ' * (((n - i) * 2) - 1) + 'O' + ' ' * i + '\n'
-        if i > 1:
+        if i == n - 1:
             break
     for i in reversed(range(0, n + 1)):
         if i == n:
@@ -49,23 +49,13 @@ def vis4(n):
             continue
         result += ' ' * i + 'O' + ' ' * (n - i) + ' ' * (n - i - 1) + 'O' + ' ' * i + '\n'
 
-    # # row, col = '', ''
-    # O = 'O'
-    # S = ' '
-    # for row in reversed(range(1, n + 1, 2)):
-    #     print(row)
-    #     for col in range(1, n + 1, 2):
-    #         result += O + S * row + O
-
-
-
     return result
 
 
 
 print('\nvisual 4\n\n')
 # [print(vis4(n)) for n in range(4)]
-n = 3
+n = 8
 print(vis4(n))
 
 
