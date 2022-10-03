7d9a12c86fba1d65a45154cc33533830ec7fb414
Robb <56496611+robb666@users.noreply.github.com>
2022-10-03 16:31:37 +0200

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -26,6 +26,26 @@ class Queue:
     def size(self):
         return len(self.buffer)
 
+    # def __iter__(self):
+    #     return QueueIterator(self)
+
+
+class QueueIterator:
+    def __init__(self, queue):
+        self._queue = queue
+        self._index = 0
+
+    def __next__(self):
+        if self._index < (len(self._queue.buffer)):
+            if self._index < len(self._queue.buffer):
+                result = (self._queue.buffer[self._index])
+            else:
+                result = (self._queue.buffer[self._index - len(self._queue.buffer)])
+
+            self._index += 1
+            return result
+        raise StopIteration
+
 
 # pq = Queue()
 #
@@ -55,17 +75,19 @@ import concurrent.futures
 
 
 def order(orders):
-    q = Queue()
+    # q = Queue()
+    q = []
     for dish in orders:
         print(f'New order: {dish}')
-        q.enqueue(dish)
+        # q.enqueue(dish)
+        q.append(dish)
         sleep(.5)
-        # yield q
+        yield q
 
 
 def serve(q):
-    while q.size() != 0:
-        dish = q.dequeue()
+    while len(q) != 0:
+        dish = q.pop(0)
         sleep(2)
         print(f'{dish} ready!')
 
@@ -74,6 +96,9 @@ orders = ['pizza', 'samosa', 'pasta', 'biryani', 'burger']
 
 funk = [order, serve]
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.map(order, [orders])
 
+with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    f1 = executor.map(order, [orders])
+    for item in f1:
+        for item2 in item:
+            f2 = executor.submit(serve, item2)
