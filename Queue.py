1e98592c5a175268c3023b8df98323344a4c2579
robb666 <56496611+robb666@users.noreply.github.com>
2022-10-07 00:01:55 +0200

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -34,6 +34,12 @@ class Queue:
     def size(self):
         return len(self.buffer)
 
+    # def front(self):
+    #     return self.buffer[0]
+
+    def front(self):
+        return self.buffer[-1]
+
     # def __iter__(self):
     #     return QueueIterator(self)
 
@@ -86,32 +92,32 @@ class QueueIterator:
 #     t2.join()
 
 
-def order(q, orders):
-    for dish in orders:
-        q.put(dish)
-        sleep(.5)
-        print(f'New order: {dish}')
-
-
-def serve(q):
-    while q.qsize() != 0:
-        dish = q.get()
-        sleep(2)  # preparing time
-        print(f'{dish} ready!')
-
-
-orders = ['pizza', 'samosa', 'pasta', 'biryani', 'burger']
-
-q = queue.Queue()
-
-t1 = threading.Thread(target=order, args=(q, orders))
-t2 = threading.Thread(target=serve, args=(q,))
-
-t1.start()
-t2.start()
-
-t1.join()
-t2.join()
+# def order(q, orders):
+#     for dish in orders:
+#         q.put(dish)
+#         sleep(.5)
+#         print(f'New order: {dish}')
+#
+#
+# def serve(q):
+#     while q.qsize() != 0:
+#         dish = q.get()
+#         sleep(2)  # preparing time
+#         print(f'{dish} ready!')
+#
+#
+# orders = ['pizza', 'samosa', 'pasta', 'biryani', 'burger']
+#
+# q = queue.Queue()
+#
+# t1 = threading.Thread(target=order, args=(q, orders))
+# t2 = threading.Thread(target=serve, args=(q,))
+#
+# t1.start()
+# t2.start()
+#
+# t1.join()
+# t2.join()
 
 
 
@@ -124,3 +130,104 @@ t2.join()
         # for q in qq:
             # print(q)
         # f2 = executor.submit(serve, q)
+
+
+
+# ex. 2
+
+
+# ex. 2
+
+
+# Ver. 4 theirs
+def produce_binary_numbers(n):
+    pass
+
+
+produce_binary_numbers(10)
+
+
+# # Ver. 3
+# def sequence():
+#     q, q2 = Queue(), Queue()
+#
+#     zero, one = '0', '1'
+#
+#     q.enqueue(one), q2.enqueue(one)
+#
+#     for _ in range(10):
+#         i = q.dequeue()
+#         q.enqueue(i + zero), q2.enqueue(i + zero)
+#
+#         j = str(int(i + zero) + 1)
+#         q.enqueue(j), q2.enqueue(j)
+#
+#         bin_num = q2.dequeue()
+#         print(bin_num)
+#
+#
+# q = Queue()
+# sequence()
+
+
+# # Ver. 2
+# def sequence(q):
+#     zero, one = '0', '1'
+#
+#     q.enqueue(one)
+#     q.enqueue(one + zero)
+#     q.enqueue(one + one)
+#     q.enqueue(one + zero + zero)
+#     q.enqueue(one + zero + one)
+#     q.enqueue(one + one + zero)
+#     q.enqueue(one + one + one)
+#     q.enqueue(one + zero + zero + zero)
+#     q.enqueue(one + zero + zero + one)
+#     q.enqueue(one + zero + one + zero)
+#
+#     for _ in range(10):
+#         print(q.dequeue())
+#
+#
+# q = Queue()
+# sequence(q)
+
+
+
+# # Ver. 1
+# def sequence():
+#     zero, one = '0', '1'
+#
+#     q = [one]
+#
+#     for i in q:
+#         i += zero
+#         q.append(i)
+#         j = q[-1]
+#         j = str(int(j) + 1)
+#         q.append(j)
+#
+#         if len(q) > 9:
+#             for j in q:
+#                 print(j)
+#             break
+#
+#
+# sequence()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
